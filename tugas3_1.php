9129b4f2c5eabb1f987c94673d37ac549a2c48f7
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-10-07 15:09:14 +0700

    Add files via upload

---
+++ tugas3_1.php
@@ -0,0 +1,17 @@
+<?php
+$jarak = 360;
+$kec = 90;
+$waktu1 = $jarak / $kec;
+$waktu2 = $waktu1 * 60;
+
+echo "Jarak Kota A- Z = ", $jarak, " Km";
+echo "<br>Kecepatan Motor = ", $kec, " Km/Jam";
+echo "<br>Waktu tempuh = ?";
+
+
+
+echo "<br><br>Jawaban : ", $waktu2, " Menit";
+
+
+
+?>
---
+++ tugas3_2.php
@@ -0,0 +1,17 @@
+<?php
+$tawal = 150000;
+$bunga = 12.5 / 100;
+$jbunga = $tawal * $bunga;
+$takhir = $tawal + $jbunga;
+
+echo "Tabungan Awal = Rp. ", $tawal;
+echo "<br>Bunga Setahun = 12.5 %";
+echo "<br>Total Tabungan Setahun = ?";
+
+
+
+echo "<br><br>Jawaban : Rp. ", $takhir;
+
+
+
+?>
